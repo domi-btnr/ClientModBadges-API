e575c444c5d5c9d5dd958b7d98643ad1efef9c70
Dominik <dominik.buettner1711@gmail.com>
2022-12-31 12:03:32 +0100

    Fix actions

--- scripts/getAliucordBadges.mjs
+++ scripts/getAliucordBadges.mjs
@@ -1,13 +1,18 @@
+import dotenv from "dotenv";
 import fetch from "node-fetch";
+
+dotenv.config();
 
 import * as utils from "./utils.mjs";
 const { addUser, CLIENT_MODS } = utils;
 const baseUrl = "https://api.github.com/repos/Aliucord/badges/contents/users";
+const token = process.env.GITHUB_TOKEN;
 
 (async () => {
-    const response = await fetch(baseUrl);
+    const response = await fetch(baseUrl, { headers: { "Authorization": `Token ${token}` } });
     const data = await response.json();
-    const jsonFiles = data?.filter(file => file.name.endsWith(".json"));
+    if (!Array.isArray(data)) return;
+    const jsonFiles = data.filter(file => file.name.endsWith(".json"));
     const promises = jsonFiles.map(async file => {
         const userId = file.name.replace(".json", "");
         const response = await fetch(file.download_url);
