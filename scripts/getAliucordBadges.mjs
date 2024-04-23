8c4a38a866d29a04db53de033d788dafa0ce3000
nyx <60797172+verticalsync@users.noreply.github.com>
2024-04-24 01:37:16 +0300

    Update aliucord badge fetching to new api

--- scripts/getAliucordBadges.mjs
+++ scripts/getAliucordBadges.mjs
@@ -1,37 +1,36 @@
-import dotenv from "dotenv";
 import axios from "axios";
-
-dotenv.config();
-
 import * as utils from "./utils.mjs";
-const { addUser, CLIENT_MODS } = utils;
-const baseUrl = "https://api.github.com/repos/Aliucord/badges/contents/users";
-const token = process.env.GITHUB_TOKEN;
-let attempts = 1;
 
-const transformData = (data) => {
-    const roles = data.roles;
-    const custom = data.custom || [];
-    const transformedCustom = custom.map(badge => ({ name: badge.text, badge: badge.url }));
-    return [...roles, ...transformedCustom];
-};
+const { addUser, CLIENT_MODS } = utils;
+
+let attempts = 1;
 
 const getAliucordBadges = async () => {
     try {
-        const response = await axios.get(baseUrl, { headers: { Authorization: `Token ${token}`, "Cache-Control": "no-cache" } });
-        if (response.status !== 200) return;
-        const data = response.data;
-        if (!Array.isArray(data)) return;
-        const jsonFiles = data.filter(file => file.name.endsWith(".json"));
-        const promises = jsonFiles.map(async file => {
-            const userId = file.name.replace(".json", "");
-            const response = await axios.get(file.download_url);
-            const data = response.data;
-            addUser(userId, CLIENT_MODS.ALIUCORD, transformData(data));
+        const { data: donorData } = await axios.get(
+            "https://aliucord.com/files/badges/data.json",
+            { headers: { "Cache-Control": "no-cache" } }
+        );
+
+        const donors = Object.entries(donorData.users).map(([id, badges]) => {
+            const { roles } = badges;
+            const customBadges = badges.custom ? Object.entries(badges.custom).map(([_, badge]) => ({ name: badge.text, badge: badge.url })) : [];
+
+            const badgeList = [...roles, ...customBadges];
+
+            return {
+                id,
+                badges: badgeList,
+            };
         });
-        await Promise.all(promises);
+
+        donors.forEach(user =>
+            addUser(user.id, CLIENT_MODS.ALIUCORD, user.badges)
+        );
+        console.log(donors);
     } catch (e) {
-        if (attempts++ > 4) console.error("Failed to get Aliucord badges after 5 attempts", e);
+        if (attempts++ > 4)
+            console.error("Failed to get Aliucord badges after 5 attempts", e);
         else setTimeout(getAliucordBadges, 500);
     }
 };
